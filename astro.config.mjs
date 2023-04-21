f3fc1fe7f9e1a0d4ce77487a351d0fc95252bf7f
Sam Johnson <samueljoh@gmail.com>
2023-04-21 11:19:20 -0600

    Add the partytown integration to your astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,18 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 
 export default defineConfig({
   site: "https://astroship.web3templates.com",
   integrations: [
     tailwind(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
